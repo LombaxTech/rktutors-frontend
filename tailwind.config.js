68c6e33f434f6efb5ed7fa87dbd528e71cf91c1a
Rakib <rakib@think-engineer.com>
2022-07-27 22:10:15 +0100

    config tailwind to be desktop first

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,23 @@ module.exports = {
   ],
   theme: {
     extend: {},
+
+    screens: {
+      sm: { max: "640px" },
+      // => @media (min-width: 640px) { ... }
+
+      md: { max: "768px" },
+      // => @media (min-width: 768px) { ... }
+
+      lg: { max: "1024px" },
+      // => @media (min-width: 1024px) { ... }
+
+      xl: { max: "1280px" },
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [require("tailwind-scrollbar"), require("daisyui")],
   daisyui: {
