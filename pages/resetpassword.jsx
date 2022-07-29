a53de08a11fbf32e1eb923d6279f078ea24c822e
Rakib <rakib@think-engineer.com>
2022-07-29 13:13:32 +0100

    added password reset

---
+++ pages/resetpassword.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from "react";
+
+import {
+  Button,
+  FormControl,
+  Flex,
+  Heading,
+  Input,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { firebaseApp } from "../firebase/firebaseClient";
+
+const auth = getAuth(firebaseApp);
+
+export default function ResetPassword() {
+  const [email, setEmail] = useState("");
+
+  const sendResetEmail = async () => {
+    try {
+      console.log(email);
+      await sendPasswordResetEmail(auth, email);
+      console.log("sent email");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack
+        spacing={4}
+        w={"full"}
+        maxW={"md"}
+        bg={useColorModeValue("white", "gray.700")}
+        rounded={"xl"}
+        boxShadow={"lg"}
+        p={6}
+        my={12}
+      >
+        <Heading lineHeight={1.1} fontSize={{ base: "2xl", md: "3xl" }}>
+          Forgot your password?
+        </Heading>
+        <Text
+          fontSize={{ base: "sm", sm: "md" }}
+          color={useColorModeValue("gray.800", "gray.400")}
+        >
+          You&apos;ll get an email with a reset link
+        </Text>
+        <FormControl id="email">
+          <Input
+            placeholder="your-email@example.com"
+            _placeholder={{ color: "gray.500" }}
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FormControl>
+        <Stack spacing={6}>
+          <Button
+            bg={"blue.400"}
+            color={"white"}
+            _hover={{
+              bg: "blue.500",
+            }}
+            onClick={sendResetEmail}
+          >
+            Request Reset
+          </Button>
+        </Stack>
+      </Stack>
+    </Flex>
+  );
+}
