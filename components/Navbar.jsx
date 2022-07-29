6ad68e93357f568840a445151fcaf32723520df1
Rakib <rakib@think-engineer.com>
2022-07-29 13:12:47 +0100

    temporary signout function

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,3 +1,29 @@
+import useCustomAuth from "../customHooks/useCustomAuth";
+
+import { signOut, getAuth } from "firebase/auth";
+import { firebaseApp } from "../firebase/firebaseClient";
+
+const auth = getAuth(firebaseApp);
+
 export default function Navbar() {
-  return <div className="h-24 w-full bg-red-500">Navbar</div>;
+  const { user, loading } = useCustomAuth();
+
+  const signout = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="h-24 w-full bg-red-500 flex justify-between">
+      <div>nav</div>
+      {user && (
+        <button className="btn btn-secondary" onClick={signout}>
+          logout
+        </button>
+      )}
+    </div>
+  );
 }
