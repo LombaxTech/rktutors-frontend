eca5b58f83a87bd9df45164e1bd9c291412c270f
Rakib <rakib@think-engineer.com>
2022-08-04 02:04:44 +0100

    tutor home page (needs more work)

--- components/Tutor/TutorHome.jsx
+++ components/Tutor/TutorHome.jsx
@@ -1,59 +1,101 @@
-import { Alert, AlertIcon } from "@chakra-ui/react";
+import { useState } from "react";
+
+import { Alert, AlertIcon, Spinner } from "@chakra-ui/react";
 import { FaCheckCircle } from "react-icons/fa";
 
+import axios from "axios";
+import { useRouter } from "next/router";
+
+import useCustomAuth from "../../customHooks/useCustomAuth";
+
 const SetupAccount = () => {
-  return (
-    <div className=" bg-white shadow-lg p-8 prose flex flex-col gap-8">
-      <div className="flex">
-        <div className="w-6/12">
-          <img
-            src="img/personal_settings.svg"
-            alt=""
-            srcset=""
-            className="h-[300px] mx-auto"
-          />
-        </div>
-        <div className="w-6/12">
-          <div className=" mx-auto">
-            <Alert status="warning" className="flex justify-center">
-              <AlertIcon />
-              Your account is not active yet
-            </Alert>
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const { user } = useCustomAuth();
+
+  const setupGooglePermssions = async () => {
+    setLoading(true);
+    try {
+      let res = await axios.get(
+        `${process.env.NEXT_PUBLIC_SERVER}/gen-auth-link`
+      );
+      const url = res.data;
+      console.log({ url });
+      router.push(url);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  if (user) {
+    let googleSetUp = user.googleAccount.setup;
+    return (
+      <div className=" bg-white shadow-lg p-8 prose flex flex-col gap-8">
+        <div className="flex">
+          <div className="w-6/12">
+            <img
+              src="img/personal_settings.svg"
+              alt=""
+              srcset=""
+              className="h-[300px] mx-auto"
+            />
           </div>
-          <div className="text-3xl my-8 font-bold text-center">
-            Complete the following steps to <br /> activate your account
-          </div>
-          <div className="flex justify-center">
-            <ul class="steps ">
-              <li class="step">Set up Google Permissions</li>
-              <li class="step ">Set up payments</li>
-              <li class="step">Complete</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <hr className="my-3" />
-      <div className="flex justify-center">
-        <div className="flex flex-col gap-8">
-          <div className="flex justify-between gap-8 items-center text-xl font-semibold uppercase">
-            <div className="flex gap-6 items-center">
-              <FaCheckCircle style={{ color: "#E5E6E6" }} /> Set up google
-              Permissions
+          <div className="w-6/12">
+            <div className=" mx-auto">
+              <Alert status="warning" className="flex justify-center">
+                <AlertIcon />
+                Your account is not active yet
+              </Alert>
             </div>
-            <button className="btn btn-primary">Start now</button>
-          </div>
-          <div className="flex justify-between items-center text-xl font-semibold uppercase">
-            <div className="flex gap-6 items-center">
-              <FaCheckCircle style={{ color: "#E5E6E6" }} />
-              Set up Payments
+            <div className="text-3xl my-8 font-bold text-center">
+              Complete the following steps to <br /> activate your account
             </div>
-            <button className="btn btn-primary">Start now</button>
+            <div className="flex justify-center">
+              <ul class="steps ">
+                <li class={`step ${googleSetUp ? "step-primary" : ""}`}>
+                  Set up Google Permissions
+                </li>
+                <li class="step ">Set up payments</li>
+                <li class="step">Complete</li>
+              </ul>
+            </div>
           </div>
         </div>
+        <hr className="my-3" />
+        <div className="flex justify-center">
+          <div className="flex flex-col gap-8">
+            <div className="flex justify-between gap-8 items-center text-xl font-semibold uppercase">
+              <div className="flex gap-6 items-center">
+                <FaCheckCircle
+                  style={{ color: googleSetUp ? "#570DF8" : "#E5E6E6" }}
+                />{" "}
+                Set up google Permissions
+              </div>
+              <button
+                className="btn btn-primary flex gap-4"
+                onClick={setupGooglePermssions}
+                disabled={loading || googleSetUp}
+              >
+                {googleSetUp ? "Completed" : "Start Now"}
+              </button>
+            </div>
+            <div className="flex justify-between items-center text-xl font-semibold uppercase">
+              <div className="flex gap-6 items-center">
+                <FaCheckCircle style={{ color: "#E5E6E6" }} />
+                Set up Payments
+              </div>
+              <button className="btn btn-primary flex gap-4" disabled={loading}>
+                Start now
+              </button>
+            </div>
+          </div>
+        </div>
+        <hr className="my-3" />
       </div>
-      <hr className="my-3" />
-    </div>
-  );
+    );
+  }
 };
 
 export default function TutorHome() {
