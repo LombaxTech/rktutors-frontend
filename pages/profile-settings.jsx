d9003deff5f434eadc27124844ba0cf8362854b2
Rakib <rakib@think-engineer.com>
2022-08-10 01:34:56 +0100

    added profile info section + tutor check before redirect

--- pages/profile-settings.jsx
+++ pages/profile-settings.jsx
@@ -9,6 +9,7 @@ import {
   TagRightIcon,
   Alert,
   AlertIcon,
+  Textarea,
 } from "@chakra-ui/react";
 import { SmallCloseIcon, DeleteIcon } from "@chakra-ui/icons";
 
@@ -20,14 +21,17 @@ import useCustomAuth from "../customHooks/useCustomAuth";
 export default function ProfileSettings() {
   const { user, userLoading } = useCustomAuth();
 
-  if (user)
+  if (user) {
+    const isTutor = user.type === "tutor";
+
     return (
       <div>
         <Sidebar>
           <div className="flex flex-col p-8 bg-white">
             <General user={user} />
             <Password user={user} />
-            <TutoringSubjects user={user} />
+            {isTutor && <TutoringSubjects user={user} />}
+            {isTutor && <ProfileInformation user={user} />}
             <div id="availability" className="">
               Availablity
             </div>
@@ -35,6 +39,7 @@ export default function ProfileSettings() {
         </Sidebar>
       </div>
     );
+  }
 }
 
 const General = ({ user }) => {
@@ -203,6 +208,91 @@ const TutoringSubjects = ({ user }) => {
             </Alert>
           )}
         </div>
+      </div>
+      <hr className="my-4" />
+    </div>
+  );
+};
+
+const ProfileInformation = ({ user }) => {
+  const [aboutMe, setAboutMe] = useState(user.profile.aboutMe);
+  const [aboutMyLessons, setAboutMyLessons] = useState(
+    user.profile.aboutMyLessons
+  );
+
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
+  const updateProfileInformation = async () => {
+    setSuccess(false);
+    setError(false);
+
+    try {
+      const isAboutMeSame = aboutMe === user.profile.aboutMe;
+      const isAboutMyLessonsSame =
+        aboutMyLessons === user.profile.aboutMyLessons;
+
+      if (!isAboutMeSame || !isAboutMyLessonsSame) {
+        await updateDoc(doc(db, "users", user.uid), {
+          ["profile.aboutMe"]: aboutMe,
+          ["profile.aboutMyLessons"]: aboutMyLessons,
+        });
+
+        console.log("updated profile info");
+        setSuccess(true);
+        setTimeout(() => setSuccess(false), 3000);
+      }
+    } catch (error) {
+      console.log(error);
+      setError(true);
+      setTimeout(() => setError(false), 3000);
+    }
+  };
+
+  return (
+    <div id="profile-information" className="flex flex-col gap-4">
+      <h1 className="text-2xl font-semibold">Profile Information</h1>
+      <div className="flex flex-col gap-8 w-8/12">
+        {/* About Me */}
+        <div className="flex flex-col gap-2">
+          <h1 className="text-lg font-semibold">About Me</h1>
+          <Textarea
+            value={aboutMe}
+            onChange={(e) => setAboutMe(e.target.value)}
+            placeholder="Write a bit about yourself"
+            size="sm"
+          />
+        </div>
+        {/* About My Lessons */}
+        <div className="flex flex-col gap-2">
+          <h1 className="text-lg font-semibold">About My Lessons</h1>
+          <Textarea
+            value={aboutMyLessons}
+            onChange={(e) => setAboutMyLessons(e.target.value)}
+            placeholder="Write a bit about your lessons"
+            size="sm"
+          />
+        </div>
+        <div className="">
+          <button
+            className="btn btn-primary"
+            onClick={updateProfileInformation}
+          >
+            Update Profile Information
+          </button>
+        </div>
+        {error && (
+          <Alert status="error">
+            <AlertIcon />
+            An error has occurred
+          </Alert>
+        )}
+        {success && (
+          <Alert status="success">
+            <AlertIcon />
+            Profile information updated
+          </Alert>
+        )}
       </div>
       <hr className="my-4" />
     </div>
