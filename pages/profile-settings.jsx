ef643c7bb43a4104118389cfbc7096984a19db1c
Rakib <rakib@think-engineer.com>
2022-08-12 19:22:56 +0100

    created stripe dashboard section in settings

--- pages/profile-settings.jsx
+++ pages/profile-settings.jsx
@@ -11,6 +11,7 @@ import {
   AlertIcon,
   Textarea,
   Spinner,
+  Tooltip,
 } from "@chakra-ui/react";
 import { SmallCloseIcon, DeleteIcon } from "@chakra-ui/icons";
 
@@ -24,6 +25,7 @@ import {
 } from "firebase/auth";
 
 import useCustomAuth from "../customHooks/useCustomAuth";
+import axios from "axios";
 
 export default function ProfileSettings() {
   const { user, userLoading } = useCustomAuth();
@@ -40,6 +42,7 @@ export default function ProfileSettings() {
             {isTutor && <TutoringSubjects user={user} />}
             {isTutor && <ProfileInformation user={user} />}
             {isTutor && <Availablity user={user} />}
+            {isTutor && <PaymentSettings user={user} />}
           </div>
         </Sidebar>
       </div>
@@ -436,6 +439,42 @@ const Availablity = ({ user }) => {
           Account active state updated
         </Alert>
       )}
+      <hr className="my-4" />
+    </div>
+  );
+};
+
+const PaymentSettings = ({ user }) => {
+  const goToStripeDashboard = async () => {
+    try {
+      let loginLinkRes = await axios.post(
+        `${process.env.NEXT_PUBLIC_SERVER}/stripe/login-link/${user.stripeConnectedAccount.id}`
+      );
+      loginLinkRes = loginLinkRes.data;
+      const { url } = loginLinkRes;
+      window.open(url, "_blank").focus();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div id="payment-settings" className="flex flex-col gap-4">
+      <h1 className="text-2xl font-semibold">Payment Settings</h1>
+      <h1 className="">
+        View your payments and settings through your Stripe dashboard
+      </h1>
+      <Tooltip label="Stripe is a world famouse online payment processing and commerce solutions for internet businesses of all sizes.">
+        <div className="text-blue-500 font-normal cursor-pointer underline">
+          What is Stripe?
+        </div>
+      </Tooltip>
+      <button
+        className="btn btn-secondary w-3/12"
+        onClick={goToStripeDashboard}
+      >
+        Payment Dashboard
+      </button>
     </div>
   );
 };
