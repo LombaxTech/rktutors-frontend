1be836d26d58be97ca023befc50b42e9fce04e07
Rakib <rakib@think-engineer.com>
2022-09-24 22:13:58 +0100

    Boost SEO

---
+++ pages/_document.jsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
